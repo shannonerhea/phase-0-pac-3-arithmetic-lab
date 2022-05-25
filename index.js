46c3d062ba9bb9e8676e274788a25c38cb9779b6
shannone <shannone426@gmail.com>
2022-05-25 11:42:33 -0400

    update

---
+++ index.js
@@ -0,0 +1,29 @@
+1 + 80
+60 - 40
+2 * 3.4
+5.0 / 2.5
+//return parseFloat(n);
+function add(num1, num2) {
+   return num1 + num2;
+};
+function subtract(num1, num2) {
+   return num1 - num2; 
+};
+function divide(num1, num2) {
+   return num1 / num2 ;
+};
+function multiply(num1, num2) {
+    return num1 * num2 ;
+};
+function increment(n) {
+    return ++n;
+};
+function decrement(n) {
+    return --n;
+};
+function makeInt(string) {
+    return parseInt(string, 10);
+};
+function preserveDecimal(string) {
+    return parseFloat(string);
+};
